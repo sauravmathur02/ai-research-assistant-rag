d27c12e18853a94781457e9c2e4ff4d10b2cbd7e
sauravmathur02 <sauravp355@gmail.com>
2026-06-20 21:00:15 +0530

    Added PDf,DOCX text extraction

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import streamlit as st
+from pypdf import PdfReader
+from docx import Document
 
 st.set_page_config(
     page_title="AI Research Assistant",
@@ -18,4 +20,49 @@ if uploaded_files:
     st.success(f"{len(uploaded_files)} file(s) uploaded")
 
     for file in uploaded_files:
-        st.write(f"📄 {file.name}")
+
+        st.subheader(f"📄 {file.name}")
+
+        if file.name.endswith(".txt"):
+
+            content = file.read().decode("utf-8")
+
+            st.text_area(
+                "File Content",
+                content,
+                height=200
+            )
+
+        elif file.name.endswith(".pdf"):
+
+            pdf = PdfReader(file)
+
+            text = ""
+
+            for page in pdf.pages:
+                extracted = page.extract_text()
+
+                if extracted:
+                    text += extracted + "\n"
+
+            st.text_area(
+                "PDF Content Preview",
+                text[:5000],
+                height=300
+            )
+        elif file.name.endswith(".docx"):
+
+            doc = Document(file)
+
+            text = ""
+
+            for paragraph in doc.paragraphs:
+                text += paragraph.text + "\n"
+
+            st.text_area(
+                "DOCX Content Preview",
+                text[:5000],
+                height=300
+            )
+        else:
+                st.info("Preview not implemented yet.")        
